534a718b39675566351f1f2e12d56cea61b1253c
LukeN-9 <lnash3@huskers.unl.edu>
2024-08-29 11:35:21 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Luke Nash
+ * lnash3@huskers.unl.edu
+ * Date: 08/29/2024
+ *
+ * Prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Luke Nash\n");
+  return 0;
+}
